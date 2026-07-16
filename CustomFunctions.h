be92911d74734aaece78bade26cc8ddeb9e3f13d
RoxasMelvino <melvinoroxas@gmail.com>
2026-07-16 13:55:55 -0700

    declare drive function

--- CustomFunctions.h
+++ CustomFunctions.h
@@ -1,11 +1,13 @@
 #ifndef __CUSTOM_FUNCTIONS_H__
 #define __CUSTOM_FUNCTIONS_H__
 
+#include <Arduino.h>
 #include <LiquidCrystal.h>
 
-extern LiquidCrystal lcd; 
+extern LiquidCrystal lcd;
 
-void lcdInit(int numCols = 16, int numRows = 2, int cursorCol = 0, int cursorRow = 0); 
+void lcdInit(int numCols = 16, int numRows = 2, int cursorCol = 0, int cursorRow = 0);
 void lcdPrintVals(float x, float y, float w);
+void drive(float dir, float speed, int motorPwmPin);
 
-#endif // __CUSTOM_FUNCTIONS_H__
+#endif  // __CUSTOM_FUNCTIONS_H__
